75935dab46b4b638b81dda137b66e197d14ad9a3
Ankul Yadav <ankulyadav9001@gmail.com>
2024-01-16 08:52:38 +0530

    first commit

---
+++ Frontend/src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainLayout from '../components/MainLayout'
+
+function Homepage() {
+    return (
+        <MainLayout></MainLayout>
+    )
+}
+
+export default Homepage
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1565D8",
+        dark: {
+          hard: "#0D2436",
+          soft: "#183856",
+        }
+      },
+      fontFamily: {
+        opensans: ["Open Sans", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      borderWidth: {
+        10: "10px",
+      },
+    },
+  },
+  plugins: [],
+}
+
